7625cca230165c5c6d2ba42213326ccc7ce82956
Ron Jacob <generalsmoke23@gmail.com>
2023-09-17 22:09:14 +0530

    feat: Completed Test Project

---
+++ src/hooks/useProducts.tsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+
+import ProductsContext from "../context/ProductsProvider";
+import { UseProductsContextType } from "../context/ProductsProvider";
+
+const useProducts = (): UseProductsContextType => {
+  return useContext(ProductsContext);
+};
+
+export default useProducts;
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import { CartProvider } from "./context/CartProvider.tsx";
+import { ProductsProvider } from "./context/ProductsProvider";
+import Cart from "./components/Cart.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ProductsProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductsProvider>
+  </React.StrictMode>
+);
